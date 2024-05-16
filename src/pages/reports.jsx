5a7a96c3b1fa0745ae0f3d640fd600a97ae2f4c3
MarselDokubo <connectmarsel@gmail.com>
2024-05-16 10:12:47 +0100

    chore: reports ui

---
+++ src/pages/reports.jsx
@@ -0,0 +1,171 @@
+import { useState } from "react";
+import { ChatIcon } from "../components/chat-icon";
+import { Button } from "../ui/button";
+import { useRef } from "react";
+
+export function Reports() {
+    const [isVisible, setIsVisible] = useState(false)
+    const [filterIsVisible, setFilterIsVisible] = useState(false);
+
+    function toggleVisibility() {
+        setIsVisible(prev => !prev)
+    }
+    function revealFilter() {
+        setFilterIsVisible(prev => !prev)
+    }
+
+    return (
+        <div className="bg-light-400 h-screen min-w-min overflow-x-hidden">
+            <div className="font-serif font-bold text-xl md:text-2xl px-4 md:px-6 py-4 md:py-6 border-b-2 flex items-center justify-between ">
+                <p>Reports</p>
+            </div>
+            <div className="border-b-2 p-4">
+                <div className=" md:p-4 flex items-center justify-between relative">
+                    <div className="filter min-w-max">
+                        <button className="text-cyan-600" onClick={revealFilter}>
+                            <i className="font-expens expensicons expensicons-inbox mr-1 text-cyan-600 align-[-2px]">&#xE94a;</i>
+                            {filterIsVisible? "Hide Filters": "Show Filters"}
+                        </button>
+                    </div>
+                    <div className={`${!isVisible && "hidden"} expense-options bg-light-300 py-2 w-64 rounded-md absolute right-5 top-[60px] pb-6 shadow-sm`}>
+                        <h2 className="mb-4 mx-4 uppercase text-xs font-bold">Expense</h2>
+                        <p className="hover:bg-primary-100 px-4"><i className="font-expens expensicons  mr-1 text-xl text-primary-200 align-[-4px]">&#xE955;</i> Manually Create</p>
+                        <p className="hover:bg-primary-100 px-4 "><i className="font-expens expensicons  mr-1 text-xl text-primary-200 align-[-4px]">&#xE955;</i> Scan Receipt</p>
+                        <p className="hover:bg-primary-100 px-4 "><i className="font-expens expensicons  mr-1 text-xl text-primary-200 align-[-4px]">&#xE955;</i> Create Multiple</p>
+                        <p className="hover:bg-primary-100 px-4 "><i className="font-expens expensicons  mr-1 text-xl text-primary-200 align-[-4px]">&#xE955;</i> Time</p>
+
+                    </div>
+                    <div className= "new-expense min-w-max">
+                        <Button label="New Expense" className={`py-2 px-4`} onClick={toggleVisibility}/>
+                    </div>
+                </div>
+                    {filterIsVisible && <FilterOptions />}
+            </div>
+            <div className="main mx-auto md:max-w-4xl mt-24 md:flex-1 px-4 ">
+                <div className="tuxy p-8 bg-light-300 rounded-t-xl border-b-2 pb-4 shadow-md">
+                    <p className="font-serif font-bold text-base">Tuxedo</p>
+                </div>
+                <div className="rounded-b-xl bg-light-300 p-8 shadow-md md:p-14 pt-0 md:mx-auto ">
+                    <div className="flex-1 max-w-52 mx-auto">
+                        <img src="/src/assets/img/doreceipt/emptystate__reports.svg" alt="" width="620" height="565" className="object-cover"/>
+                    </div>
+                    <div className="text-center">
+                        <h2 className="font-bold text-xl mb-2 ">The current filter show 0 reports.</h2>
+                        <p className="font-normal text-xs md:text-base">Add filters or create a new report using the button above.
+                        </p>
+                    </div> 
+                </div>
+            </div>
+            <div className="fixed bottom-4 right-4 md:bottom-8 md:right-8">
+                <ChatIcon />
+            </div>
+        </div>
+    )
+}
+
+
+function FilterOptions() {
+    const [category, setCategory] = useState("All Category")
+    const [categoryIsVisible, setCategoryIsVisible] = useState(false)
+    const list = useRef(null)
+    function changeCategory({ target }) {
+        setCategory(target.dataset.value)
+          
+
+    }
+    function revealCategory(e) {
+        e && console.log("Child", e), e.stopPropagation();
+        setCategoryIsVisible(prev => !prev)
+    }
+    return (
+        <div className="md:px-4 mt-6" onClick={(e) => {
+            console.log("Parent")
+            categoryIsVisible && setCategoryIsVisible(false)
+        }}>
+            <div className="date relative pl-6 md:pl-8 md:flex gap-2">
+                <i className="absolute left-0 top-0 font-expens expensicons  mr-1 md:text-xl text-slate-400 align-[-4px]">&#xE94E;</i>
+                <div className="flex-1 flex items-center px-2 bg-white max-w-96 w-full rounded-md mb-2">
+                    <label htmlFor="from-date" className="border-r pr-2">From
+                    </label>
+                        <input type="date" name="from-date" id="from-date" className="flex-1 text-transparent focus:text-gray-600 focus:outline-none" />
+                </div>
+                <div className="flex items-center px-2 flex-1 bg-white rounded-md mb-2 max-w-96">
+                    <label htmlFor="to-date" className="border-r pr-2">To
+                    </label>
+                        <input type="date" name="to-date" id="to-date" className="flex-1 text-transparent focus:text-gray-600 focus:outline-none" />
+                </div>
+            </div>
+            <div className="search mt-5 relative pl-6 md:pl-8 items-center md:flex gap-2 ">
+                <i className="absolute left-0 top-1 font-expens expensicons  mr-1 md:text-xl text-slate-400 align-[-4px]">&#xE93C;</i>
+                <div className="bg-white rounded-md max-w-96 mb-2 md:mb-0 px-2 p-0.5 flex-1 relative text-xs">
+                        <input type="text" id="merchant" placeholder="Report ID, name or email" className="focus:text-gray-600 focus:outline-none border-r-2 w-full" />
+                        <span className="font-expens expensicons text-slate-400 ml-[6px] align-[-1px] absolute right-1">&#xE93C;</span>
+                </div>
+                <div className="relative px-1 py-0.5 bg-white mb-2 md:mb-0 max-w-96 rounded-md border flex-1">
+                        <div className="bg-white px-1 py-0.5 pl-1 border max-w-96  rounded-md relative" onClick={(e) => { !categoryIsVisible && revealCategory(e) }}>
+                            <input type="text" value={category} onChange={({target}) => setCategory(target.value)} className="w-full px-1  text-slate-900 focus:outline-none" />
+                            <i role="button" className="font-expens expensicons absolute right-2 top-1.5 text-sm text-slate-400">&#xE921;</i>
+                        </div>
+                        <ul ref={list} className={`${!categoryIsVisible && "hidden"} dropdown-menu expensify-dropdown text-slate-900 mt-1 border border-t-0 max-h-72 max-w-96 w-full overflow-y-scroll absolute bg-light-400 z-10`}>
+                            <li className={`active li-compact p-2 md:pr-2 border hover:bg-primary-100`} role="button" onClick={changeCategory} data-value="All Category">
+                                <a href="#" role="button"  data-toggle="tooltip" data-container="body" data-delay="{&quot;show&quot;:1000, &quot;hide&quot;:100}" title="" data-original-title="All categories">
+                                    <span>
+                                        <i className="expensicons expensicons-checkmark"></i>
+                                    </span>All categories
+                                </a>
+                            </li>
+                            <li className="li-compact p-2 md:pr-2 border hover:bg-primary-100" role="button" onClick={changeCategory} data-value="Uncategorized">
+                                <a href="#" role="button"  data-toggle="tooltip" data-container="body" data-delay="{&quot;show&quot;:1000, &quot;hide&quot;:100}" title="" data-original-title="Uncategorized">Uncategorized</a>
+                            </li>
+                        </ul>
+                </div>
+                <div className="relative px-1 py-0.5 bg-white mb-2 md:mb-0 max-w-96 rounded-md border flex-1">
+                        <div className="bg-white px-1 py-0.5 pl-1 border max-w-96  rounded-md relative" onClick={(e) => { !categoryIsVisible && revealCategory(e) }}>
+                            <input type="text" value={category} onChange={({target}) => setCategory(target.value)} className="w-full px-1  text-slate-900 focus:outline-none" />
+                            <i role="button" className="font-expens expensicons absolute right-2 top-1.5 text-sm text-slate-400">&#xE921;</i>
+                        </div>
+                        <ul ref={list} className={`${!categoryIsVisible && "hidden"} dropdown-menu expensify-dropdown text-slate-900 mt-1 border border-t-0 max-h-72 max-w-96 w-full overflow-y-scroll absolute bg-light-400 z-10`}>
+                            <li className={`active li-compact p-2 md:pr-2 border hover:bg-primary-100`} role="button" onClick={changeCategory} data-value="All Category">
+                                <a href="#" role="button"  data-toggle="tooltip" data-container="body" data-delay="{&quot;show&quot;:1000, &quot;hide&quot;:100}" title="" data-original-title="All categories">
+                                    <span>
+                                        <i className="expensicons expensicons-checkmark"></i>
+                                    </span>All categories
+                                </a>
+                            </li>
+                            <li className="li-compact p-2 md:pr-2 border hover:bg-primary-100" role="button" onClick={changeCategory} data-value="Uncategorized">
+                                <a href="#" role="button"  data-toggle="tooltip" data-container="body" data-delay="{&quot;show&quot;:1000, &quot;hide&quot;:100}" title="" data-original-title="Uncategorized">Uncategorized</a>
+                            </li>
+                        </ul>
+                </div>
+                                <div className="relative px-1 py-0.5 bg-white mb-2 md:mb-0 max-w-96 rounded-md border flex-1">
+                        <div className="bg-white px-1 py-0.5 pl-1 border max-w-96  rounded-md relative" onClick={(e) => { !categoryIsVisible && revealCategory(e) }}>
+                            <input type="text" value={category} onChange={({target}) => setCategory(target.value)} className="w-full px-1  text-slate-900 focus:outline-none" />
+                            <i role="button" className="font-expens expensicons absolute right-2 top-1.5 text-sm text-slate-400">&#xE921;</i>
+                        </div>
+                        <ul ref={list} className={`${!categoryIsVisible && "hidden"} dropdown-menu expensify-dropdown text-slate-900 mt-1 border border-t-0 max-h-72 max-w-96 w-full overflow-y-scroll absolute bg-light-400 z-10`}>
+                            <li className={`active li-compact p-2 md:pr-2 border hover:bg-primary-100`} role="button" onClick={changeCategory} data-value="All Category">
+                                <a href="#" role="button"  data-toggle="tooltip" data-container="body" data-delay="{&quot;show&quot;:1000, &quot;hide&quot;:100}" title="" data-original-title="All categories">
+                                    <span>
+                                        <i className="expensicons expensicons-checkmark"></i>
+                                    </span>All categories
+                                </a>
+                            </li>
+                            <li className="li-compact p-2 md:pr-2 border hover:bg-primary-100" role="button" onClick={changeCategory} data-value="Uncategorized">
+                                <a href="#" role="button"  data-toggle="tooltip" data-container="body" data-delay="{&quot;show&quot;:1000, &quot;hide&quot;:100}" title="" data-original-title="Uncategorized">Uncategorized</a>
+                            </li>
+                        </ul>
+                </div>
+            </div>
+
+            <div className="type mt-5 relative pl-6 md:pl-8 flex flex-wrap gap-2">
+                <i className="absolute left-0 top-1 font-expens expensicons mr-1 md:text-xl text-slate-400 align-[-4px]">&#xE958;</i>
+                <button role="button" className=" focus:outline-none w-32 bg-[#0185ff] text-sm font-light text-[13px] rounded-full text-slate-200 p-[5px] px-3">Open</button>
+                <button role="button" className=" focus:outline-none w-32 bg-[#003c73] text-sm font-light text-[13px] rounded-full text-slate-200 p-[5px] px-3">Processing</button>
+                <button role="button" className=" focus:outline-none w-32 bg-[#03d47c] text-sm font-light text-[13px] rounded-full text-slate-200 p-[5px] px-3">Approved</button>
+                <button role="button" className=" focus:outline-none w-32 bg-[#002e22] text-sm font-light text-[13px] rounded-full text-slate-200 p-[5px] px-3">Reimbursed</button>
+                <button role="button" className=" focus:outline-none w-32 bg-[#ebe6df] text-sm font-light text-[13px] rounded-full text-[#002e22] p-[5px] px-3">Closed</button>
+       
+            </div>
+        </div>
+    )
+}
